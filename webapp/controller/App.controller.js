d4bf221cfa5c1b67f5d037b166ac6dbb6cb5610b
https://github.com/chandeep2013/viz.git <chandeep2013@gmail.com>
2021-05-11 04:30:48 +0000

    ok

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -2,7 +2,7 @@ sap.ui.define([
 	"sap/ui/core/mvc/Controller",
 	"sap/ui/model/json/JSONModel"
 ], function (Controller, JSONModel) {
-	"use strict";
+	"use strict";   
 
 	return Controller.extend("com.bosch.hr.swift_trv.controller.App", {
 		onInit: function () {
